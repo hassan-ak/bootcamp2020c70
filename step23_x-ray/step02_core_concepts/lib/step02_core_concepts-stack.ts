8853c4867feb9756d1647c9e66f1c1d35e41d1f4
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2022-01-28 15:04:38 +0500

    Update step02_core_concepts-stack.ts

--- step23_x-ray/step02_core_concepts/lib/step02_core_concepts-stack.ts
+++ step23_x-ray/step02_core_concepts/lib/step02_core_concepts-stack.ts
@@ -1,15 +1,33 @@
 import * as cdk from '@aws-cdk/core';
-// import * as sqs from '@aws-cdk/aws-sqs';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as iam from '@aws-cdk/aws-iam';
 
 export class Step02CoreConceptsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    ///create a specific role for Lambda function
+    const role = new iam.Role(this, 'LambdaRole', {
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'Step02CoreConceptsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    ///Attaching s3 read only access to policy
+    const policy = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: ['s3:Get*', 's3:List*'],
+      resources: ['*'],
+    });
+
+    //granting IAM permissions to role
+    role.addToPolicy(policy);
+
+    new lambda.Function(this, 'lambda-s3-x-ray-tracing', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'index.handler',
+      // Enabling X-Ray Tracing
+      tracing: lambda.Tracing.ACTIVE,
+      role: role,
+    });
   }
 }
