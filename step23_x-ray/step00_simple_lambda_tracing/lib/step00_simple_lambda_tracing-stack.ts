c650aab03204eebb0594e193a332a898afaf0ced
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2022-01-28 13:12:23 +0500

    lambda function

--- step23_x-ray/step00_simple_lambda_tracing/lib/step00_simple_lambda_tracing-stack.ts
+++ step23_x-ray/step00_simple_lambda_tracing/lib/step00_simple_lambda_tracing-stack.ts
@@ -1,15 +1,17 @@
 import * as cdk from '@aws-cdk/core';
-// import * as sqs from '@aws-cdk/aws-sqs';
+import * as lambda from '@aws-cdk/aws-lambda';
 
 export class Step00SimpleLambdaTracingStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'Step00SimpleLambdaTracingQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Lambda function
+    new lambda.Function(this, 'lambda-x-ray-tracing', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'index.handler',
+      // Enabling X-Ray Tracing
+      tracing: lambda.Tracing.ACTIVE,
+    });
   }
 }
