46c459a6a5a52f07d3f8874c2863b6393b274297
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2022-01-28 14:07:32 +0500

    cdk app

---
+++ step23_x-ray/step01_lambda_with_s3_tracing/test/step01_lambda_with_s3_tracing.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as Step01LambdaWithS3Tracing from '../lib/step01_lambda_with_s3_tracing-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/step01_lambda_with_s3_tracing-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Step01LambdaWithS3Tracing.Step01LambdaWithS3TracingStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
